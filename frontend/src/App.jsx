87fcae3d40a2b3e3cecccde0a4139c6312a1910f
rajeebkm <rkm2541997@gmail.com>
2024-09-01 00:39:49 +0530

    frontend setup added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography, Box } from '@mui/material';
+function App() {
+  return (
+    <Container maxWidth="sm">
+    <Box sx={{ my: 4 }}>
+    <Typography variant="h4" component="h1" gutterBottom>
+    Stellar Swap
+    </Typography>
+    </Box>
+    </Container>
+  );
+}
+export default App;
